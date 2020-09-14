0286248d6b407ad1d761c6463a328a8fd6fe8ea3
Rodrigo Giannotti <giannottirodrigo@gmail.com>
2020-09-14 11:03:33 -0300

    Include multi search
    Cria a função busca_multi, que faz a procura por multiplos CEPs,
    respeitando o intervalo mínimo de 1 segundo entre cada procura, e
    retorna as informações encontradas em uma tibble. Para que ela funcione
    pequenas mudanças na função parse.api foram feitas para que as colunas
    da tibble criada sempre fossem criadas com tipos de variáveis coerentes
    (similar ao que já se havia feito nas colunas latitude e longitude)

---
+++ R/busca_multi.R
@@ -0,0 +1,20 @@
+#' @title Busca por CEP
+#' @description Busca usando multiplos CEP (search for information using multiple postal code).
+#' @importFrom magrittr %>%
+#' @importFrom magrittr %<>%
+#' @importFrom dplyr add_row
+#' @param lista_ceps lista de CEPs (postal code list).
+#' @param token Token de autorização. Veja <http://cepaberto.com/users/register>.
+#' @export
+
+busca_multi <- function( lista_ceps = c("01001000", "01001001") , token = NULL ) {
+  CEPS <- parse_api(NA) %>% na.exclude()
+  lista_ceps %<>%  as.vector()
+  for (i in lista_ceps) {
+    CEPS %<>% add_row(
+      busca_cep(cep = i, token = token)
+    )
+    Sys.sleep(1)
+  }
+  return(CEPS)
+}
